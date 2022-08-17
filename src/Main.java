6907dd07432971b056dde0976b4f31e8d11f81a7
arkadiilavrentev <arkasha.lavrentev@bk.ru>
2022-08-17 21:50:01 +0300

    UTF-8

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,19 @@ public class Main {
     public static void main(String[] args) {
         int[] arr = generateRandomArray();
 
-        // Задача 1
+        // Р—Р°РґР°С‡Р° 1
 
-        System.out.println("Задача 1");
+        System.out.println("Р—Р°РґР°С‡Р° 1");
         int month = 0;
         for (int i = 0; i < arr.length; i++) {
             month += arr[i];
         }
-        System.out.println("Сумма трат за месяц составила " + month + " рублей.");
+        System.out.println("РЎСѓРјРјР° С‚СЂР°С‚ Р·Р° РјРµСЃСЏС† СЃРѕСЃС‚Р°РІРёР»Р° " + month + " СЂСѓР±Р»РµР№.");
         System.out.println();
 
-        // Задача 2
+        // Р—Р°РґР°С‡Р° 2
 
-        System.out.println("Задача 2");
+        System.out.println("Р—Р°РґР°С‡Р° 2");
         int min = 200_000;
         int max = 100_000;
         for (int i = 0; i < arr.length; i++) {
@@ -27,20 +27,20 @@ public class Main {
                 max = arr[i];
             }
         }
-        System.out.println("Минимальная сумма трат за день составила " + min + " рублей.");
-        System.out.println("Максимальная сумма трат за день составила " + max + " рублей.");
+        System.out.println("РњРёРЅРёРјР°Р»СЊРЅР°СЏ СЃСѓРјРјР° С‚СЂР°С‚ Р·Р° РґРµРЅСЊ СЃРѕСЃС‚Р°РІРёР»Р° " + min + " СЂСѓР±Р»РµР№.");
+        System.out.println("РњР°РєСЃРёРјР°Р»СЊРЅР°СЏ СЃСѓРјРјР° С‚СЂР°С‚ Р·Р° РґРµРЅСЊ СЃРѕСЃС‚Р°РІРёР»Р° " + max + " СЂСѓР±Р»РµР№.");
         System.out.println();
 
-        // Задача 3
+        // Р—Р°РґР°С‡Р° 3
 
-        System.out.println("Задача 3");
+        System.out.println("Р—Р°РґР°С‡Р° 3");
         float middleSum = month / 30;
-        System.out.println("Средняя сумма трат за месяц составила " + middleSum + " рублей.");
+        System.out.println("РЎСЂРµРґРЅСЏСЏ СЃСѓРјРјР° С‚СЂР°С‚ Р·Р° РјРµСЃСЏС† СЃРѕСЃС‚Р°РІРёР»Р° " + middleSum + " СЂСѓР±Р»РµР№.");
         System.out.println();
 
-        // Задача 4
+        // Р—Р°РґР°С‡Р° 4
 
-        System.out.println("Задача 4");
+        System.out.println("Р—Р°РґР°С‡Р° 4");
         char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
         for (int i = reverseFullName.length; i > 0; i--) {
             System.out.print(reverseFullName[i - 1]);
@@ -48,9 +48,9 @@ public class Main {
         System.out.println();
         System.out.println();
 
-        // Задача 5
+        // Р—Р°РґР°С‡Р° 5
 
-        System.out.println("Задача 5");
+        System.out.println("Р—Р°РґР°С‡Р° 5");
         int[][] matrix = new int[3][3];
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
@@ -67,9 +67,9 @@ public class Main {
         }
         System.out.println();
 
-        // Задача 6
+        // Р—Р°РґР°С‡Р° 6
 
-        System.out.println("Задача 6");
+        System.out.println("Р—Р°РґР°С‡Р° 6");
         int[] array = {5, 4, 3, 2, 1};
         int[] massiv = new int[array.length];
         System.out.println(Arrays.toString(array));
@@ -82,9 +82,9 @@ public class Main {
         System.out.println(Arrays.toString(array));
         System.out.println();
 
-        // Задача 7
+        // Р—Р°РґР°С‡Р° 7
 
-        System.out.println("Задача 7");
+        System.out.println("Р—Р°РґР°С‡Р° 7");
         int[] m = {6, 3, 5, 7, 11};
         System.out.println(Arrays.toString(m));
         for (int i = 0; i < m.length / 2; i++) {
@@ -95,9 +95,9 @@ public class Main {
         System.out.println(Arrays.toString(m));
         System.out.println();
 
-        // Задача 8
+        // Р—Р°РґР°С‡Р° 8
         {
-            System.out.println("Задача 8");
+            System.out.println("Р—Р°РґР°С‡Р° 8");
             int[] numbers = {-6, 2, 5, -8, 8, 10, 4, -7, 12, 1};
             int sum = 0;
             for (int i = 0; i < numbers.length; i++) {
@@ -114,9 +114,9 @@ public class Main {
             }
             System.out.println();
         }
-        // Задача 9
+        // Р—Р°РґР°С‡Р° 9
 
-        System.out.println("Задача 9");
+        System.out.println("Р—Р°РґР°С‡Р° 9");
         int[] numbers = {-6, 2, 5, -8, 8, 10, 4, -7, 12, 1};
         int sum = 0;
         for (int i = 0; i < numbers.length; i++){
